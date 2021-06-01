506661a9dc88e675a4c2cba0195e7934c9acc7d6
Claudio Altamura <claudio@claudioaltamura.de>
2021-06-01 13:13:43 +0200

    new: current version

---
+++ src/main/java/de/claudioaltamura/quarkus/HelloWorldResource.java
@@ -0,0 +1,16 @@
+package de.claudioaltamura.quarkus;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/helloworlds")
+public class HelloWorldResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
---
+++ src/test/java/de/claudioaltamura/quarkus/NativeHelloWorldResourceIT.java
@@ -0,0 +1,9 @@
+package de.claudioaltamura.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeHelloWorldResourceIT extends HelloWorldResourceTest {
+
+    // Execute the same tests but in native mode.
+}
